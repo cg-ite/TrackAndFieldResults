08c56b7c2cb5d5272413ae979354c412ec5dd689
cg-ite <cg-ite@gmx.de>
2025-08-21 23:04:55 +0200

    feat: New Seltec AthonClient
    - New Results Provider Seltec
    - Thanks for the colaboration OSP Hessen

---
+++ src/TrackAndFieldResults/Seltec/AthonEvent.cs
@@ -0,0 +1,88 @@
+﻿/*
+ * SPDX - FileCopyrightText: Copyright © 2021 Olympiastützpunkt Hessen <cguenther@lsbh.de>
+ * SPDX - License - Identifier: GPL - 3.0 - or - later
+ */
+
+using System.Text.Json.Serialization;
+
+namespace TrackAndFieldResults.Seltec
+{
+	/// <summary>
+	/// Custom extentions of the AthonEvent
+	/// </summary>
+	public partial class AthonEvent : IEquatable<AthonEvent>, ICloneable
+	{
+		/// <summary>
+		/// Formated string of all agegroups of the Event
+		/// </summary>
+		[JsonIgnore]
+		public string FormatedAgegroups
+		{
+			get
+			{
+				if (Agegroups.Count > 0)
+				{
+					return string.Join(", ", Agegroups.Select(a => a.Shortcode));
+				}
+				return string.Empty;
+			}
+		}
+
+		/// <summary>
+		/// Umfassende Darstellung für toString()
+		/// </summary>
+		[JsonIgnore]
+		public string Label
+		{
+			get
+			{
+				int cHeats = GetHeats().Count() - 1;
+				int cSquads = GetSquads().Count() - 1;
+				string sHeat = cHeats > 0 ? $" [L: {cHeats}] " : "";
+				string sSquad = cSquads > 0 ? $"[R: {cSquads}]" : "";
+				return $"{Longname} - {FormatedAgegroups}{sHeat}{sSquad}";
+			}
+		}
+
+		public object Clone()
+		{
+			var ev = (AthonEvent)MemberwiseClone();
+			return ev;
+		}
+
+		public bool Equals(AthonEvent other)
+		{
+			return Object.Equals(other.Label, Label);
+		}
+
+		public override int GetHashCode()
+		{
+			return Label.GetHashCode();
+		}
+
+		public override string ToString()
+		{
+			return Label;
+		}
+
+		/// <summary>
+		/// Gibt die Riegen eines einzelnen Wettkampfes zurück
+		/// </summary>
+		/// <param name="event"></param>
+		/// <returns></returns>
+		public IEnumerable<string> GetSquads()
+		{
+			return Entries.GroupBy(ev => ev.Squad).Select(g => g.Key);
+		}
+
+		/// <summary>
+		/// Gibt die Läufe eines einzelnen Wettkampfes zurück
+		/// </summary>
+		/// <param name="event"></param>
+		/// <returns></returns>
+		public IEnumerable<string> GetHeats()
+		{
+			return Entries.GroupBy(ev => ev.Heat).Select(g => g.Key);
+		}
+	}
+}
---
+++ src/TrackAndFieldResults/Seltec/AthonShortcodes.cs
@@ -0,0 +1,107 @@
+﻿/*
+ * SPDX - FileCopyrightText: Copyright © 2021 Olympiastützpunkt Hessen <cguenther@lsbh.de>
+ * SPDX - License - Identifier: GPL - 3.0 - or - later
+ */
+
+namespace TrackAndFieldResults.Seltec
+{
+    /// <summary>
+    /// Enum Klasse für die Shortcodes
+    /// eines Events
+    /// </summary>
+    /// <remarks>Maybe not complete</remarks>
+    public static class AthonShortcode
+    {
+        /// <summary>
+        /// Stabhochsprung
+        /// </summary>
+        public const string Polevault = "PV";
+        /// <summary>
+        /// Weitsprung
+        /// </summary>
+        public const string Longjump = "LJ";
+        /// <summary>
+        /// Dreisprung
+        /// </summary>
+        public const string Triplejump = "TJ";
+        /// <summary>
+        /// Hochsprung
+        /// </summary>
+        public const string Highjump = "HJ";
+
+        /// <summary>
+        /// 110m Hürden
+        /// </summary>
+        public const string Hurdles110 = "11H";
+        /// <summary>
+        /// 100m Hürden
+        /// </summary>
+        public const string Hurdles100 = "10H";
+        /// <summary>
+        /// 60m Hürden
+        /// </summary>
+        public const string Hurdles60 = "60H";
+
+        /// <summary>
+        /// 60m Sprint
+        /// </summary>
+        public const string Running60 = "60M";
+
+
+        /// <summary>
+        /// 100m Sprint
+        /// </summary>
+        public const string Running100 = "100";
+
+        /// <summary>
+        /// 200m Sprint
+        /// </summary>
+        public const string Running200 = "200";
+
+        /// <summary>
+        /// 400m Sprint
+        /// </summary>
+        public const string Running400 = "400";
+        /// <summary>
+        /// 800m Mittelstrecke
+        /// </summary>
+        public const string Running800 = "800";
+
+        /// <summary>
+        /// 1500m Mittelstrecke
+        /// </summary>
+        public const string Running1500 = "1k5";
+        /// <summary>
+        /// Siebenkampf
+        /// </summary>
+        public const string Heptatlon = "HEP";
+        /// <summary>
+        /// Hallen 5-Kampf der Frauen
+        /// </summary>
+        public const string Indoor5k = "5HK";
+        /// <summary>
+        /// Hallen 7-Kampf der Männer
+        /// </summary>
+        public const string Indoor7k = "7-KH";
+        /// <summary>
+        /// Zehnkampf
+        /// </summary>
+        public const string Decathlon = "DEC";
+        /// <summary>
+        /// Kugelstoßen
+        /// </summary>
+        public const string Shortput = "SP";
+        /// <summary>
+        /// Speerwurf
+        /// </summary>
+        public const string JavelinThrow = "JT";
+        /// <summary>
+        /// Hammerwurf
+        /// </summary>
+        public const string HammerThrow = "HT";
+        /// <summary>
+        /// Diskuswurf
+        /// </summary>
+        public const string DiscusThrow = "DT";
+    }
+}
