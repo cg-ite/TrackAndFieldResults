5b25a3ad9a4edc17ae0851f40096d73115c17dbc
cguenther <cguenther@lsbh.de>
2026-06-03 16:47:38 +0200

    feat: Entry wird doch benötigt
    - damit können startlisten für gemischte Wettkämpfe berechnet werden
      zb WU20 und W

---
+++ src/TrackAndFieldResults/Common/Entry.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * SPDX - FileCopyrightText: Copyright © 2025 Christian Günther <cg-ite@gmx.de>
+ * SPDX - License - Identifier: GPL - 3.0 - or - later
+ */
+
+using TrackAndFieldResults.Seltec;
+
+namespace TrackAndFieldResults.Common
+{
+    /// <summary>
+    /// Die Teilnahme eines Athleten einer Disziplin.
+    /// Notwendig um den Fall abzubilden, dass mehrere
+    /// AgeGroups in einem Wettkampf starten oder dass
+    /// eine Athlet hochgemeldet wird.
+    /// Hat auch Einfluss auf die Sortierung der Start- und
+    /// Ergebnisliste.
+    /// </summary>
+    public partial class Entry : IEntry
+    {
+        public string Id { get ; set ; }
+        public string CompetitorId { get ; set ; }
+        public string AgegroupId { get ; set ; }
+        public string Squad { get ; set ; }
+        public Attempt[] Attempts { get ; set ; }= Array.Empty<Attempt>();
+        public string Lane { get ; set ; }
+        public string Heat { get ; set ; }
+        public string Startposition { get ; set ; }
+        public EntryState State { get ; set ; }
+
+        public static Entry FromEntry(AthonEntry entry, Type type)
+        {
+            Entry ent = new Entry();
+            ent.Id = entry.Id;
+            ent.CompetitorId = entry.CompetitorId;
+            ent.AgegroupId = entry.AgegroupId;
+            ent.Squad = entry.Squad;
+            if (entry.State == AthonEntryState.Finished)
+            {
+                ent.Attempts =
+                    entry.Attempts.Select(a => Attempt.FromIntermediate(a, entry.CompetitorId, type)).ToArray();
+            }
+            ent.Lane = entry.Lane;
+            ent.Heat = entry.Heat;
+            if (entry.State.HasValue)
+            {
+                ent.State = (EntryState)entry.State.Value;
+            }
+            return ent;
+        }
+
+        public override string ToString()
+        {
+            return $"{CompetitorId} ({State}) L:{Lane} H: {Heat} V:{Attempts.Count()}";
+        }
+    }
+}
---
+++ src/TrackAndFieldResults/Common/IEntry.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * SPDX - FileCopyrightText: Copyright © 2025 Olympiastützpunkt Hessen <cguenther@lsbh.de>
+ * SPDX - License - Identifier: GPL - 3.0 - or - later
+ * code was sent as patch, no public git repo available
+ */
+namespace TrackAndFieldResults.Common
+{
+    /// <summary>
+    /// Die Teilnahme eines Athleten einer Disziplin
+    /// </summary>
+    public interface IEntry
+    {
+        /// <summary>ID inside Document or Database</summary>
+        string Id { get; set; }
+        /// <summary>ID reference to competitor</summary>
+        string CompetitorId { get; set; }
+        /// <summary>ID reference to agegroup (must be part of this event)</summary>
+        string AgegroupId { get; set; }
+        /// <summary>Name or number of squad ('Riege')</summary>
+        string Squad { get; set; }
+        /// <summary>List of attempts or single performance. Can be omitted of this is only an entry before event start</summary>
+        Attempt[] Attempts { get; set; }
+        /// <summary>Lane Number or Number of original Starting order in technical event. If lane is double set use 1/1 and 1/2 for lane 1 positions</summary>
+        string Lane { get; set; }
+        /// <summary>Name or number of heat. If omitted but RoundType is present: competitor should be DNS/CAN</summary>
+        string Heat { get; set; }
+        /// <summary>
+        /// Position in technical events
+        /// </summary>
+        string Startposition { get; set; }
+        /// <summary>
+        /// Zeigt den Status des Athleten im Wettkampf
+        /// </summary>
+        EntryState State { get; set; }
+    }
+
+    /// <summary>
+    /// State eine Athleten in einer Disziplin
+    /// Vorher None, nach dem Wettkampf
+    /// entsprechend der Leistung
+    /// </summary>
+    public enum EntryState
+    {
+        None = 0,
+        Set = 1,
+        Finished = 2,
+        Dnf = 3,
+        Can = 4,
+        Dns = 5,
+        Dsq = 6,
+    }
+}
